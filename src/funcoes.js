12ee2abefba1611b8c41c5af3539249a73044abe
Rick504 <rick.wes1@gmail.com>
2023-01-30 18:39:33 -0300

    teste comprimentar concluido

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,17 +1,22 @@
 /**
  * Implemente uma função que receba o nome de um cliente e devolve um cumprimento para ele.
- * 
- * EXEMPLO: 
+ *
+ * EXEMPLO:
  *   - Se o nome do cliente é Bill Gates, o cumprimento deve ser "Olá, Bill Gates!".
  *   - Se o nome do cliente é Steve Jobs, o cumprimento deve ser "Olá, Steve Jobs!".
  */
-function cumprimenta() {}
+
+function cumprimenta(nome) {
+    return `Olá, ${nome}!`
+}
+
+cumprimenta("Jorge")
 
 
 /**
  * Implemente uma função que recebe o valor de uma compra e a quantidade de itens comprados,
- * e retorne o valor do desconto de acordo com a quantidade de itens. 
- * 
+ * e retorne o valor do desconto de acordo com a quantidade de itens.
+ *
  * A TABELA DE DESCONTO É:
  *   - 1 item: 0 de desconto;
  *   - 2 itens: 3% de desconto;
@@ -24,7 +29,7 @@ function calculaDesconto() {}
 
 /**
  * Implemente uma função que receba um número X e devolva a soma dos número de 1 a X.
- * 
+ *
  * EXEMPLO:
  *   - X é 100: calcula 1 + 2 + 3 + ... + 99 + 100, retorna 5050
  *   - X é 200: calcula 1 + 2 + 3 + ... + 199 + 200, retorna 20100
@@ -34,7 +39,7 @@ function somatorio() {}
 
 /**
  * Implemente uma função que recebe um número N devolve o fatorial do número.
- * 
+ *
  * EXEMPLO:
  *   - n é 5: 5! = 120
  *   - n é 9: 9! = 362880
@@ -49,7 +54,7 @@ function fatorial() {}
 function equacaoDeSegundoGrau() {}
 
 /**
- * Implemente uma função que receba uma data no formato DD/MM/YYYY, 
+ * Implemente uma função que receba uma data no formato DD/MM/YYYY,
  * extraia o dia, mês e ano, e retorne um array com:
  *   - o ANO na posição 1.
  *   - o MÊS na posição 2.
@@ -61,7 +66,7 @@ function extraiElementosDaData() {}
 /**
  * Considerando a sequência de Fibonacci 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...,
  * implemente uma função que receba a enésima posição da sequência, e devolva o algarismo correspondente.
- * 
+ *
  * EXEMPLO:
  *   - posição é 7: retorna o elemento 13.
  *   - posição é 8: retorna o elemento 21.
@@ -77,12 +82,12 @@ function fibonacci() {}
 
 
 /**
- * O financeiro do Comex disponibilizou os faturamentos mensais numa lista de registros. 
- * Cada registro tem duas posições: uma string com o nome do mês e o valor vendido nele. 
- * 
+ * O financeiro do Comex disponibilizou os faturamentos mensais numa lista de registros.
+ * Cada registro tem duas posições: uma string com o nome do mês e o valor vendido nele.
+ *
  * Implemente uma função que receba uma lista de faturamentos (exemplo abaixo) e retorne
- * o valor anual consolidado. 
- * 
+ * o valor anual consolidado.
+ *
  * EXEMPLO DA ESTRUTURA: [
  *                         ['janeiro', 10],
  *                         ['fevereiro', 20],
@@ -95,18 +100,18 @@ function calculaFaturamentoAnual() {}
 
 /**
  * Agora a equipe do financeiro gostaria de filtrar os faturamentos pra ver períodos específicos.
- * 
- * Implemente uma função que receba uma lista de faturamentos e um callback que recebe dois parâmetros 
+ *
+ * Implemente uma função que receba uma lista de faturamentos e um callback que recebe dois parâmetros
  * para filtrar se aquele registro vai entrar no cálculo ou não.
- * 
- * 
- * EXEMPLO DO FILTRO: callback(mes, valor) 
+ *
+ *
+ * EXEMPLO DO FILTRO: callback(mes, valor)
  */
 function calculaFaturamentoAnualComFiltro() {}
 
 
 /**
- * Crie uma função que receba o fator de multiplicação de uma tabuada específica e retorne uma 
+ * Crie uma função que receba o fator de multiplicação de uma tabuada específica e retorne uma
  * função que calcule a multiplicação do fator por um número.
  */
 function criaTabuada() {
