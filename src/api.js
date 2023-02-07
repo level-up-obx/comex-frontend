0c665d893b96dc7f9b56f7197e7f873e375f6970
Rick504 <rick.wes1@gmail.com>
2023-02-07 13:48:43 -0300

    criando funções criarCliente, salvaCliente e listaCliente

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 const categorias = []
 
-export function salvaCategorias(newCategorias) { return categorias.push(newCategorias) }
+export function salvaCategorias(novaCategorias) { return categorias.push(novaCategorias) }
 export function listaCategorias() { return categorias }
 
 const produtos = []
 
-export function salvaProdutos(newProdutos) { return produtos.push(newProdutos) }
+export function salvaProdutos(novoProduto) { return produtos.push(novoProduto) }
 export function listaProdutos() { return produtos }
+
+const clientes = []
+
+export function salvaClientes(novoCliente) { return clientes.push(novoCliente) }
+export function listaClientes() { return clientes }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { criaCategoria, criaProduto } from './modelo.js'
-import { salvaCategorias, listaCategorias, salvaProdutos, listaProdutos } from './api.js'
+import { criaCategoria, criaProduto, criaCliente } from './modelo.js'
+import {
+    salvaCategorias,
+    salvaProdutos,
+    salvaClientes,
+    listaCategorias,
+    listaProdutos,
+    listaClientes
+} from './api.js'
 
 // Categorias
 const INFORMÁTICA = criaCategoria('INFORMÁTICA' ,'2023/06/21' , 'ATIVA')
@@ -15,6 +22,7 @@ salvaCategorias(LIVROS)
 // console.log(`${MÓVEIS.id}: ${MÓVEIS.nome} (${MÓVEIS.status + ' - ' + MÓVEIS.criacao})`)
 // console.log(`${LIVROS.id}: ${LIVROS.nome} (${LIVROS.status + ' - ' + LIVROS.criacao})`)
 
+console.log('Lista Categorias',listaCategorias())
 
 // Produtos
 const notebook_samsung = criaProduto('Notebook Samsung', null, 3523.00, 1, 'Informática')
@@ -25,5 +33,55 @@ salvaProdutos(notebook_samsung)
 salvaProdutos(clean_architecture)
 salvaProdutos(monitor_dell)
 
-console.log('Lista Categorias',listaCategorias())
 console.log('Lista Produtos',listaProdutos())
+
+//Cliente
+const Cliente1 = criaCliente(
+    'Cristiano',
+    'Ronaldo',
+    '333.333.333-33',
+    '(11) 99999-9999',
+    {
+        rua: 'rua tal',
+        numero: 12,
+        bairro: 'bairro tal',
+        cidade: 'cidade tal',
+        estado: 'são paulo',
+        cep: 456456213,
+        complemento: 'perto da padaria'
+    })
+
+const Cliente2 = criaCliente(
+    'Vini',
+    'Junior',
+    '333.333.333-33',
+    '(11) 99999-9999',
+    {
+        rua: 'rua tal',
+        numero: 12,
+        bairro: 'bairro tal',
+        cidade: 'cidade tal',
+        estado: 'são paulo',
+        cep: 456456213,
+        complemento: 'perto da padaria'
+    })
+const Cliente3 = criaCliente(
+    'Luiz',
+    'Henrique',
+    '333.333.333-33',
+    '(11) 99999-9999',
+    {
+        rua: 'rua tal',
+        numero: 12,
+        bairro: 'bairro tal',
+        cidade: 'cidade tal',
+        estado: 'são paulo',
+        cep: 456456213,
+        complemento: 'perto da padaria'
+    })
+
+salvaClientes(Cliente1)
+salvaClientes(Cliente2)
+salvaClientes(Cliente3)
+
+console.log('Lista Clientes',listaClientes())
