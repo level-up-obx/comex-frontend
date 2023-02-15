38a16377b3fa93326a4a170edc7be8764662e3c0
Bruno <bruno.goncalves@onebox.one>
2023-02-15 08:52:46 -0300

    Tira seletores do dom de dentro das funções

--- src/pagina/clientes.js
+++ src/pagina/clientes.js
@@ -8,19 +8,21 @@ const bairro = document.querySelector('#bairro')
 const cidade = document.querySelector('#cidade')
 const numero = document.querySelector('#numero')
 
-clienteBtn.onclick = () => {  
-  const nome = document.querySelector('#nome')
-  const sobrenome = document.querySelector('#sobrenome')
-  const cpf = document.querySelector('#cpf')
-  const telefone = document.querySelector('#telefone')
+const nome = document.querySelector('#nome')
+const sobrenome = document.querySelector('#sobrenome')
+const cpf = document.querySelector('#cpf')
+const telefone = document.querySelector('#telefone')
 
+const cep = document.querySelector('#cep')
+
+clienteBtn.onclick = () => {
   if (!nome.value || !sobrenome.value || !cpf.value || !logradouro.value || !bairro.value || !cidade.value || !numero.value) {
     alert("Preencha todos os campor antes de enviar.")
     return
   }
   
   const endereco = {
-    logradouro :logradouro.value,
+    logradouro: logradouro.value,
     bairro: bairro.value,
     cidade: cidade.value,
     numero: numero.value
@@ -35,7 +37,9 @@ clienteBtn.onclick = () => {
 
 cepBtn.onclick = async (e) => {
   e.preventDefault()
-  const cep = document.querySelector('#cep')
+  
+  if (!cep.value) return
+
   const resp = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
   const endereco = await resp.json()
 
