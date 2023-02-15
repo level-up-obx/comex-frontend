934b20ff8073e20a81a3e912ad478c65b52c3ab9
Rick504 <rick.wes1@gmail.com>
2023-02-15 14:46:42 -0300

    autoComplete em endereço

--- src/pagina/clientes.js
+++ src/pagina/clientes.js
@@ -30,3 +30,27 @@ formClientes.addEventListener('submit', function (event) {
 
     console.log(novoCliente)
 })
+
+async function searchAddress(cep) {
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+
+    let response = await fetch(url)
+    return await response.json()
+}
+
+let inputCpf = document.getElementById("cep");
+
+inputCpf.onblur = async function(event){
+    let enderecoViaCep = await searchAddress(event.target.value)
+
+    document.getElementById('logradouro').value = enderecoViaCep.logradouro
+    document.getElementById('complemento').value = enderecoViaCep.complemento
+    document.getElementById('uf').value = enderecoViaCep.uf
+    document.getElementById('bairro').value = enderecoViaCep.bairro
+    document.getElementById('localidade').value = enderecoViaCep.localidade
+
+    // console.log(enderecoViaCep)
+}
+
+
+
