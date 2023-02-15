9815077c6887447019b3ed2af7199e03792929f1
Rick504 <rick.wes1@gmail.com>
2023-02-15 12:44:36 -0300

    listando produtos

--- src/pagina/produtos.js
+++ src/pagina/produtos.js
@@ -1,1 +1,18 @@
 import { criaProduto } from '../modelo.js'
+
+let formProdutos = document.querySelector('#formProdutos')
+
+formProdutos.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let novaProduto = criaProduto(
+        event.target.elements['nome'].value,
+        event.target.elements['descricao'].value,
+        parseInt(event.target.elements['preco'].value) || null,
+        parseInt(event.target.elements['quantidade'].value) || null,
+        parseInt(event.target.elements['categoria'].value) || null
+    )
+
+
+    console.log(novaProduto)
+})
