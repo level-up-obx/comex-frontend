a9126a00c44f17a9c1a7d2f5ee0d946b058028aa
João Victor <joao.silva@onebox.one>
2023-02-17 15:40:43 -0300

    Finalizacao da Pagina de Clientes

--- src/pages/cliente.js
+++ src/pages/cliente.js
@@ -1,37 +1,78 @@
-import { createCategory } from "../modelo.js";
+import { createClient } from "../modelo.js";
 
-const firstName = document.getElementById("");
-const lastName = document.getElementById("");
-const CPF = document.getElementById("");
-const telephone = document.getElementById("");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const cpf = document.getElementById("cpf");
+const telephone = document.getElementById("telephone");
 const cep = document.getElementById("cep");
-const btnCep = document.getElementById("getCep")
+const btnSend = document.getElementById("client-button")
 
 const street = document.getElementById("street")
-const number = document.getElementById("houseNumber")
+const number = document.getElementById("number")
+const complement = document.getElementById("complement")
 const district = document.getElementById("district")
 const city = document.getElementById("city")
-const cityState = document.getElementById("cityState")
+const state = document.getElementById("state")
 
-const getAddress = async (cep) => {
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
-    street.value = data.logradouro;
-    district.value = data.bairro;
-    city.value = data.localidade;
-    cityState.value = data.uf;
-    const address = {
-        street: data.logradouro,
-        district: data.bairro,
-        city:data.localidade,
-        cityState:data.uf,
-    }
-}
-
-btnCep.addEventListener("click", (event) => {
+cep.addEventListener("blur", (event) => {
     event.preventDefault();
     getAddress(cep.value)
 })
+btnSend.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const address = {
+        street: street.value,
+        number: number.value,
+        complement: complement.value,
+        district: district.value,
+        city: city.value,
+        state: state.value
+    }
+    createClient(firstName.value, lastName.value, cpf.value, telephone.value, address)
+})
+btnSend.addEventListener("click", (event) => {
+    event.preventDefault();
+    const address = {
+        street: street.value,
+        number: number.value,
+        complement: complement.value,
+        district: district.value,
+        city: city.value,
+        state: state.value
+    }
+    console.log(createClient(firstName.value, lastName.value, cpf.value, telephone.value, address))
+})
+
+const getError = (error) => {
+    throw new Error("Ocorreu um erro!", { cause: error });
+};
+
+const getAddress = async (cep) => {
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    try {
+        const response = await fetch(url, {mode: 'no-cors'});
+        const data = await response.json();
+        // console.log(data)
+        if(data.logradouro != "" || data.logradouro != null) {
+            street.value = data.logradouro;
+            street.readOnly = true;
+        }
+        if(data.bairro != "" || data.bairro != null) {
+            district.value = data.bairro;
+            district.readOnly = true;
+        }
+        if(data.localidade != "" || data.localidade != null) {
+            city.value = data.localidade;
+            city.readOnly = true;
+        }
+        if(data.uf != "" || data.uf != null) {
+            state.value = data.uf;
+            state.readOnly = true;
+        }
+    } catch (error) {
+        getError(error)
+    }
+}
+
+
 
