15b6be4a6a538157b171fa49f54dd8d724f86a55
Rick504 <rick.wes1@gmail.com>
2023-03-07 10:59:40 -0300

    corrigindo config do vite e acertando caminho dos arquivos

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-// vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'src');
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(root, 'index.html'),
-        categorias: resolve(root, 'pages/categorias/index.html'),
-        produtos: resolve(root, 'pages/produtos/index.html'),
-        clientes: resolve(root, 'pages/clientes/index.html'),
-      },
-    },
-  },
-})
+    root: root,
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(root, 'index.html'),
+                categorias: resolve(root, 'pages/categorias/index.html'),
+                produtos: resolve(root, 'pages/produtos/index.html'),
+                clientes: resolve(root, 'pages/clientes/index.html')
+            },
+        }
+    }
+});
